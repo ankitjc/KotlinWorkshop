56e725a93c1a8ba3910c68d244154a4960882618
Ankit Chaudhari <ankitjc@gmail.com>
2024-02-27 17:29:23 +0000

    Infix function

---
+++ src/Lab-7-InFixFunctions.kt
@@ -0,0 +1,19 @@
+package src
+
+fun main()
+{
+//    var a =true
+//    var b =false
+//    var result: Boolean;
+//
+//   result = a || b
+//    result = a.or(b)
+
+//   result = a or b
+//    println("The result is $result")
+
+    var x = 10 add 20
+    println("Sum is $x")
+}
+
+infix fun Int.add(b:Int):Int = this + b;
